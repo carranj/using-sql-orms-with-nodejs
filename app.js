0c6a59e36ae17d0d648af23f847f7862385b59a3
carranj <carranco.jose.r@gmail.com>
2020-05-25 23:41:05 -0500

    Define a Model

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const sequelize = new Sequelize({
     storage: 'movies.db'
 });
 
+// Movie model
+class Movie extends Sequelize.Model {}
+Movie.init({
+    title: Sequelize.STRING
+}, { sequelize }); // same as { sequelize: sequelize });
+
 // async IIFE
 (async () => {
+    // Sync all tables
+    await sequelize.sync({ force: true });
 
     try {
-        await sequelize.authenticate();
-        console.log('Connection to the database successful!');
+    // Instance of the Movie class represents a database row
+    const movie = await Movie.create({
+        title: 'Toy Story',
+      });
+      
+    console.log(movie.toJSON());
+        
     } catch (error) {
       console.error('Error connecting to the database: ', error);
     }
